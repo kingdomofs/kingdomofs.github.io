0c93abf26429787a39a0e89f2159fd587b362afe
Sandeep Tripathy <kingdomofs@users.noreply.github.com>
2017-06-12 17:13:21 +0530

    Create builtbys.js

--- builtbys.js
+++ builtbys.js
@@ -1,5 +1,5 @@
 /*Light version - for dark backgrounds*/
-document.getElementById("built-by-s-logo-w").innerHTML = '<span style="color:#fafafa">Built by</span><br/><a style="text-decoration:none !important;" title="Sandeep Tripathy" href="https://www.KingdomofS.com" target="_blank"><img style="filter:invert(100%);height:30px;" src="https://www.kingdomofs.com/img/logo.png"></a>'
+document.getElementById("built-by-s-logo-w").innerHTML = '<span style="color:#fafafa">Built by</span><br/><a style="text-decoration:none !important;" title="Sandeep Tripathy" href="https://www.KingdomofS.com" target="_blank"><img style="filter:invert(100%);padding-bottom:20px;height:30px;" src="https://www.kingdomofs.com/img/logo.png"></a>'
 
 /*Dark version - for light backgrounds*/
-document.getElementById("built-by-s-logo-b").innerHTML = '<span style="color:#212121">Built by</span><br/><a style="text-decoration:none !important;" title="Sandeep Tripathy" href="https://www.KingdomofS.com" target="_blank"><img style="height:30px;" src="https://www.kingdomofs.com/img/logo.png"></a>';
+document.getElementById("built-by-s-logo-b").innerHTML = '<span style="color:#212121">Built by</span><br/><a style="text-decoration:none !important;" title="Sandeep Tripathy" href="https://www.KingdomofS.com" target="_blank"><img style="height:30px;padding-bottom:20px;" src="https://www.kingdomofs.com/img/logo.png"></a>';
